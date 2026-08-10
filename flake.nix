34d4ebc2514f2184a901fb2e598de5c95a18a482
Alexander Mills <alex@oresoftware.com>
2026-08-10 02:42:53 -0500

    build(DEN-3028): pin canonical ores-sops in Nix shell

--- flake.nix
+++ flake.nix
@@ -2,15 +2,24 @@
   description = "Meta-agent Rust daemon and Leptos control-plane development shell";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.ores-sops.url =
+    "github:ORESoftware/ores-sops/bcedd169490775d58f418a59248a3e2354451cf2";
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, ores-sops }:
     let
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in {
       devShells = forAllSystems (system:
-        let pkgs = import nixpkgs { inherit system; };
-        in { default = import ./.nix/devshell.nix { inherit pkgs; }; });
+        let
+          pkgs = import nixpkgs { inherit system; };
+          oresSops = ores-sops.packages.${system}.default;
+        in {
+          default = import ./.nix/devshell.nix {
+            inherit pkgs oresSops;
+            oresSopsShellHook = ores-sops.lib.shellHook;
+          };
+        });
 
       formatter = forAllSystems (system: (import nixpkgs { inherit system; }).nixfmt-rfc-style);
     };
